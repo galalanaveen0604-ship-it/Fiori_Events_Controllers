0cc660488afa6e0fba4c5a3a6dff4e7af08d7219
Galala Naveen <galalanaveen0604@gmail.com>
2026-01-31 05:55:10 +0000

    update

--- webapp/controller/Events_Controller.controller.js
+++ webapp/controller/Events_Controller.controller.js
@@ -7,60 +7,87 @@ sap.ui.define([
     return Controller.extend("org.index.it.sapfiori.controller.Events_Controller", {
         onInit() {
         },
-        onPlus() {
+        onClick(oEvent) {
             var digit1 = this.getView().byId("_idGenInput").getValue();
             var num1 = Number(digit1);
 
             var digit2 = this.getView().byId("_idGenInput1").getValue();
             var num2 = Number(digit2);
+            var btnText = oEvent.getSource().getProperty("text");
 
-            var Output = num1 + num2;
+            if (btnText == "Plus") {
+                var Output = num1 + num2;
+            }
+            if (btnText == "Minus") {
+                var Output = num1 - num2;
+            }
+            if (btnText == "Multiplication") {
+                var Output = num1 * num2;
+            }
+            if (btnText == "Division") {
+                var Output = num1 / num2;
+            }
 
-            MessageBox.information("Plus:" + Output)
+            MessageBox.information("Value:" + Output)
             this.getView().byId("_idGenInput").setValue();
             this.getView().byId("_idGenInput1").setValue();
 
-        },
-        onMinus() {
-            var digit1 = this.getView().byId("_idGenInput").getValue();
-            var num1 = Number(digit1);
-
-            var digit2 = this.getView().byId("_idGenInput1").getValue();
-            var num2 = Number(digit2);
-
-            var Output = num1 - num2;
-
-            MessageBox.information("Minus:" + Output)
-            this.getView().byId("_idGenInput").setValue();
-            this.getView().byId("_idGenInput1").setValue();
-        },
-
-        onMultiplication() {
-            var digit1 = this.getView().byId("_idGenInput").getValue();
-            var num1 = Number(digit1);
-
-            var digit2 = this.getView().byId("_idGenInput1").getValue();
-            var num2 = Number(digit2);
-
-            var Output = num1 * num2;
-
-            MessageBox.information("Multiply:" + Output)
-            this.getView().byId("_idGenInput").setValue();
-            this.getView().byId("_idGenInput1").setValue();
-        },
-
-        onDivision() {
-            var digit1 = this.getView().byId("_idGenInput").getValue();
-            var num1 = Number(digit1);
-
-            var digit2 = this.getView().byId("_idGenInput1").getValue();
-            var num2 = Number(digit2);
-
-            var Output = num1 / num2;
-
-            MessageBox.information("Divison:" + Output)
-            this.getView().byId("_idGenInput").setValue();
-            this.getView().byId("_idGenInput1").setValue();
         }
+        // onPlus() {
+        //     var digit1 = this.getView().byId("_idGenInput").getValue();
+        //     var num1 = Number(digit1);
+
+        //     var digit2 = this.getView().byId("_idGenInput1").getValue();
+        //     var num2 = Number(digit2);
+
+        //     var Output = num1 + num2;
+
+        //     MessageBox.information("Plus:" + Output)
+        //     this.getView().byId("_idGenInput").setValue();
+        //     this.getView().byId("_idGenInput1").setValue();
+
+        // },
+        // onMinus() {
+        //     var digit1 = this.getView().byId("_idGenInput").getValue();
+        //     var num1 = Number(digit1);
+
+        //     var digit2 = this.getView().byId("_idGenInput1").getValue();
+        //     var num2 = Number(digit2);
+
+        //     var Output = num1 - num2;
+
+        //     MessageBox.information("Minus:" + Output)
+        //     this.getView().byId("_idGenInput").setValue();
+        //     this.getView().byId("_idGenInput1").setValue();
+        // },
+
+        // onMultiplication() {
+        //     var digit1 = this.getView().byId("_idGenInput").getValue();
+        //     var num1 = Number(digit1);
+
+        //     var digit2 = this.getView().byId("_idGenInput1").getValue();
+        //     var num2 = Number(digit2);
+
+        //     var Output = num1 * num2;
+
+        //     MessageBox.information("Multiply:" + Output)
+        //     this.getView().byId("_idGenInput").setValue();
+        //     this.getView().byId("_idGenInput1").setValue();
+        // },
+
+        // onDivision() {
+        //     var digit1 = this.getView().byId("_idGenInput").getValue();
+        //     var num1 = Number(digit1);
+
+        //     var digit2 = this.getView().byId("_idGenInput1").getValue();
+        //     var num2 = Number(digit2);
+
+        //     var Output = num1 / num2;
+
+        //     MessageBox.information("Divison:" + Output)
+        //     this.getView().byId("_idGenInput").setValue();
+        //     this.getView().byId("_idGenInput1").setValue();
+        // }
+        
     });
 });
